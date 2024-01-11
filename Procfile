d094ae66c311fb6f09567ed55cffa0a0f87fcff6
dinakidane <dinakidane@hotmail.com>
2024-01-11 14:44:48 +0000

    add heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn coffee-lovers-dj.wsgi
--- coffeelover/coffeelover/settings.py
+++ coffeelover/coffeelover/settings.py
@@ -27,9 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=False
 
-ALLOWED_HOSTS = ['coffee-lovers-dj.herokuapp.com', 'localhost' '8000-dinakidane-coffeelovers-h24jlgr5ylx.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['coffee-lovers-dj.herokuapp.com', 'localhost' '8000-dinakidane-coffeelovers-h24jlgr5ylx.ws-eu107.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
