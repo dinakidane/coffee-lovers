321ad16a78ac1aa3f75adad105b02a645bb9b427
dinakidane <dinakidane@hotmail.com>
2024-02-01 17:51:51 +0000

    fix models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'published')
+    list_filter = ('published',)
+    search_fields = ('title', 'author__username')
+    actions = ['publish_posts', 'unpublish_posts']
+
+    def publish_posts(self, request, queryset):
+        queryset.update(published=True)
+    publish_posts.short_description = "Publish selected posts"
+
+    def unpublish_posts(self, request, queryset):
+        queryset.update(published=False)
+    unpublish_posts.short_description = "Unpublish selected posts"
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at', 'approved')
+    list_filter = ('approved',)
+    search_fields = ('user__username', 'post__title', 'coffee_experience')
+    actions = ['approve_comments', 'disapprove_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Approve selected comments"
+
--- main/models.py
+++ main/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=1),
     coffee_experience = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} - {self.created_at}'
